f7e786e0fc6eb70269fd41eba720b72929cfa3c3
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2023-10-18 18:55:42 +0200

    finished second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const rootRef = document.querySelector('.gallery');
+rootRef.addEventListener('click', handleItemClick);
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+      </a>
+    </li>`
+  )
+  .join('');
+
+rootRef.insertAdjacentHTML('beforeend', markup);
+
+function handleItemClick(event) {
+  event.preventDefault();
+
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
